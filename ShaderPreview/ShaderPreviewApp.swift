e560f4db9c3d3f1e129d2311662c18a60bd2814d
yinglun <duan.yinglun@gmail.com>
2020-12-18 12:48:42 +0800

    update ui

--- ShaderPreview/ShaderPreviewApp.swift
+++ ShaderPreview/ShaderPreviewApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ShaderPreviewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(renderer: ImageRenderer())
         }
     }
 }
--- ShaderPreview/Views/ContentView.swift
+++ ShaderPreview/Views/ContentView.swift
@@ -8,35 +8,112 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var renderer: ImageRenderer
+
     @State private var fullText: String = ""
+    
+    @State private var showsInfoView = false
 
     var body: some View {
-        VStack {
-            HStack {
-                TextEditor(text: $fullText)
-                    .frame(height: 44)
-                Spacer()
-                Button("Open resources folder", action: {
-                    NSWorkspace.shared.open(ResourcesFolder.folderURL)
-                })
+        Group {
+            if showsInfoView {
+                ZStack {
+                    mainView()
+                    VStack {
+                        Spacer()
+                        infoView()
+                    }
+                }
+            } else {
+                mainView()
             }
-                .padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 20))
-            StaticImageRenderingView(renderer: ImageRenderer(), errorHandler: { error in
+        }
+            .frame(idealWidth: 800, idealHeight: 600)
+    }
+    
+    private func mainView() -> some View {
+        func topBar() -> some View {
+            HStack() {
+                Spacer()
+                Button(action: {
+                    NSWorkspace.shared.open(ResourcesFolder.folderURL)
+                }, label: {
+                    Image(systemName: "folder")
+                        .imageScale(.large)
+                })
+                    .buttonStyle(PlainButtonStyle())
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+                .frame(height: 30)
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        }
+        
+        func previewView() -> some View {
+            MetalPetalView(mtiContext: renderer.context, mtiImage: $renderer.image, errorHandler: { error in
                 self.fullText = error?.localizedDescription ?? ""
             })
         }
-            .frame(minWidth: 0,
-                   idealWidth: 800,
-                   maxWidth: .greatestFiniteMagnitude,
-                   minHeight: 0,
-                   idealHeight: 600,
-                   maxHeight: .greatestFiniteMagnitude
-            )
+        
+        func bottomBar() -> some View {
+            HStack {
+                Button(action: {
+                    showsInfoView = true
+                }, label: {
+                    HStack {
+                        Image(systemName: "exclamationmark.triangle")
+                            .imageScale(.small)
+                        Text(fullText.isEmpty ? "0" : fullText.replacingOccurrences(of: "\n", with: ""))
+                    }
+                })
+                    .buttonStyle(PlainButtonStyle())
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 3, trailing: 20))
+
+
+                Spacer()
+            }
+                .frame(height: 22)
+        }
+        
+        return VStack(spacing: 0) {
+            topBar()
+            previewView()
+            bottomBar()
+        }
+    }
+    
+    
+    private func infoView() -> some View {
+        func topBar() -> some View {
+            HStack() {
+                Spacer()
+                Button(action: {
+                    showsInfoView = false
+                }, label: {
+                    Image(systemName: "xmark")
+                        .imageScale(.medium)
+                })
+                    .buttonStyle(PlainButtonStyle())
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+            }
+                .frame(height: 30)
+        }
+        
+        return VStack {
+            Rectangle()
+                .fill(Color(NSColor.windowBackgroundColor))
+                .frame(height: 1)
+            topBar()
+            TextEditor(text: $fullText)
+                .frame(height: 186)
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        }
+            .background(Color(NSColor.controlBackgroundColor))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(renderer: ImageRenderer())
     }
 }
